3f901b2fc344a0e538f6ddfe718806131a04de32
Zhaoyan <irisfeng0321@outlook.com>
2022-02-03 20:40:15 -0500

    Initial commit

---
+++ server.ts
@@ -0,0 +1,24 @@
+/**
+ * @file Server file
+ */
+import express, {Request, Response} from 'express';
+import mongoose from "mongoose";
+mongoose.connect('mongodb+srv://irisfeng:Aa970321@cluster0.enbum.mongodb.net/tuiter?retryWrites=true&w=majority');
+
+import bodyParser from "body-parser";
+import UserController from "./controllers/UserController";
+import TuitController from "./controllers/TuitController";
+const app = express();
+app.use(bodyParser.json())
+
+app.get('/', (req: Request, res: Response) =>
+    res.send('Welcome!'));
+
+app.get('/add/:a/:b', (req: Request, res: Response) =>
+    res.send(req.params.a + req.params.b));
+
+const userController = UserController.getInstance(app);
+const tuitController = TuitController.getInstance(app);
+
+const PORT = 4000;
+app.listen(process.env.PORT || PORT);
